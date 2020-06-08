d6f9533b3542bb8e7b25a3b95e123ee60bab13a8
bidipyne <pyne.bidisha2017@gmail.com>
2020-06-08 14:06:05 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,url_for,request
+import pickle
+
+filename = 'C:\\Users\\Admin\\Desktop\\Data Science 2020\\NLP\\SMSCollection\\nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('C:\\Users\\Admin\\Desktop\\Data Science 2020\\NLP\\SMSCollection\\tranform.pkl','rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+	app.run()
